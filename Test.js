971a39a7179cc47025295b58d553e37e6b8a5da6
Sudhansu2001 <90862782+Sudhansu2001@users.noreply.github.com>
2025-01-09 12:11:10 +0530

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World! Welcome to cloud APP Service');
+}).listen(8080);
